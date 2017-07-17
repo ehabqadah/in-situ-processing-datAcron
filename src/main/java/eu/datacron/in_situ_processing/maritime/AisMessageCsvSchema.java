b8ddeacb84dd3e19978fc26c9016d5a43bd3f3af
eqadah <ehab.qadah@iais.fraunhofer.de>
2017-07-17 16:53:12 +0200

    Added an option to add new line with serilaztion value of a single AIS message.

--- src/main/java/eu/datacron/in_situ_processing/maritime/AisMessageCsvSchema.java
+++ src/main/java/eu/datacron/in_situ_processing/maritime/AisMessageCsvSchema.java
@@ -7,8 +7,9 @@ import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.streaming.util.serialization.DeserializationSchema;
 import org.apache.flink.streaming.util.serialization.SerializationSchema;
-import org.apache.log4j.Logger;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import eu.datacron.in_situ_processing.common.utils.ReflectionUtils;
 
@@ -22,17 +23,29 @@ public class AisMessageCsvSchema implements SerializationSchema<AisMessage>,
     DeserializationSchema<AisMessage> {
 
   private static final long serialVersionUID = 4339578918900034257L;
-  private static final Logger logger = Logger.getLogger(AisMessageCsvSchema.class.getName());
+  private static final Logger logger = LoggerFactory.getLogger(AisMessageCsvSchema.class.getName());
   private transient JSONObject parsingJsonConfigs;
   private String parsingJsonConfigsStr;
   String delimiter;
+  private boolean addNewLine;
 
   public AisMessageCsvSchema() {}
 
+
+  public AisMessageCsvSchema(String parsingJsonConfigsStr, boolean addNewLine) {
+    setupSchema(parsingJsonConfigsStr, addNewLine);
+  }
+
   public AisMessageCsvSchema(String parsingJsonConfigsStr) {
+    setupSchema(parsingJsonConfigsStr, false);
+  }
+
+  private void setupSchema(String parsingJsonConfigsStr, boolean addNewLine) {
     this.parsingJsonConfigsStr = parsingJsonConfigsStr;
+    this.addNewLine = addNewLine;
     initParsingConfigObject();
   }
+
 
   private void initParsingConfigObject() {
     // make sure that parsing config object is initialized
@@ -49,7 +62,12 @@ public class AisMessageCsvSchema implements SerializationSchema<AisMessage>,
 
   @Override
   public byte[] serialize(AisMessage element) {
-    return element.toCsv(delimiter).getBytes(StandardCharsets.UTF_8);
+    String uniformDelimiter = ",";
+    String csvLine = element.toCsv(uniformDelimiter);
+    if (addNewLine) {
+      csvLine += "\n";
+    }
+    return csvLine.getBytes(StandardCharsets.UTF_8);
   }
 
   @Override
@@ -59,6 +77,12 @@ public class AisMessageCsvSchema implements SerializationSchema<AisMessage>,
     return parseCSVline(csvLine);
   }
 
+  /**
+   * 
+   * @param csvLine
+   * 
+   * @return an AisMessage for valid csv line or null in case of any parsing error
+   */
   private AisMessage parseCSVline(String csvLine) {
     initParsingConfigObject();
     AisMessage aisMessage = new AisMessage();
@@ -70,21 +94,24 @@ public class AisMessageCsvSchema implements SerializationSchema<AisMessage>,
 
       // check if the JSON has a corresponding key for the given field
       if (!parsingJsonConfigs.isNull(fieldName)) {
-        // Get value of the field from the csv line based on its index
-        int fieldIndex = parsingJsonConfigs.getInt(fieldName);
-        // Casr the string value of the field based on its acutal type
-        Object castedFieldValue =
-            ReflectionUtils.getCastedFieldValue(field, fieldsValue[fieldIndex]);
-
         try {
+          // Get value of the field from the csv line based on its index
+          int fieldIndex = parsingJsonConfigs.getInt(fieldName);
+          // Casr the string value of the field based on its acutal type
+          Object castedFieldValue =
+              ReflectionUtils.getCastedFieldValue(field, fieldsValue[fieldIndex]);
+
+
           // set the value of the field from the csv line using reflection
           field.set(aisMessage, castedFieldValue);
-        } catch (IllegalArgumentException | IllegalAccessException e) {
+        } catch (Exception e) {
           logger.error(e.getMessage());
+          // invalid line
+          return null;
         }
       }
     }
-    
+
     return aisMessage;
   }
 
